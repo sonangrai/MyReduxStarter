b0356f00d05eef937f68b9709e290e292f3373b8
Sonahang Rai <shonahangrae@gmail.com>
2021-03-10 11:42:10 +0545

    Added the main App entrypoint

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet";
+
+function App() {
+  return (
+    <>
+      <Helmet>
+        <title>React Starter</title>
+      </Helmet>
+      <h1>Welcome TO Redux Starter</h1>
+    </>
+  );
+}
+
+export default App;
