7810fbd2865ed454e4c70a72c813672922dd6204
Sonahang Rai <shonahangrae@gmail.com>
2021-03-21 11:22:54 +0545

    Removed Movie Reducers

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import alertReducers from "./alertReducers";
 import authReducer from "./authReducers";
-import movieReducers from "./movieReducers";
 
 export default combineReducers({
   auth: authReducer,
   alert: alertReducers,
-  movie: movieReducers,
 });
